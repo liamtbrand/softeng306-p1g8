8e1e7bb13657d731e713344f901699c8c095b10c
Robert Rewcastle <robbierew@gmail.com>
2018-07-24 15:46:38 +1200

    added test for TaskGraphBuilder

--- src/test/se306group8/scheduleoptimizer/taskgraph/TaskGraphBuilderTest.java
+++ src/test/se306group8/scheduleoptimizer/taskgraph/TaskGraphBuilderTest.java
@@ -2,15 +2,60 @@ package se306group8.scheduleoptimizer.taskgraph;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TaskGraphBuilderTest {
 
-	@Test
-	void testTestNameThing() {
-		fail("Add tests here");
+	private TaskGraph smallGraph;
+	
+	@BeforeEach
+	void createGraph() {
+		TaskGraphBuilder builder = new TaskGraphBuilder();
+		builder.addTask("a", 2)
+		       .addTask("b", 3)
+		       .addTask("c", 3)
+		       .addTask("d", 2)
+		       .addDependecy("a", "b", 1)
+		       .addDependecy("a", "c", 2)
+		       .addDependecy("b", "d", 2)
+		       .addDependecy("c", "d", 1);
+		smallGraph = builder.buildGraph();
 	}
 	
 	@Test
-	void testPassingTestNameThing() {  }
+	void testFourNodeTaskGraph() {
+			
+		Iterator<Task> rootNodes =  smallGraph.getRoots().iterator();
+		Task root = rootNodes.next();
+		
+		//Only one root node
+		assertFalse(rootNodes.hasNext());
+		
+		assertEquals(root.getName(), "a");
+		assertEquals(root.getCost(), 2);
+		
+		Collection<Dependency> rootChildren = root.getChildren();
+		
+		//Two dependencies
+		assertEquals(rootChildren.size(), 2);
+		
+		Dependency dep = rootChildren.iterator().next();
+		
+		assertEquals(dep.getParent(), root);
+		assertEquals(dep.getChild().getName(),"d");
+		assertEquals(dep.getChild().getCost(),"1");
+				
+	}
+	
+//	@Test
+//	void testTopologicalOrder() {
+//		List<Task> topologicalOrder = smallGraph.getAll();
+//		
+//		assertEqual(topologicalOrder.get(0))
+//	}
 }
