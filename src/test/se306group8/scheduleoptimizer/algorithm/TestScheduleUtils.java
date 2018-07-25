70dcbdaf4e98fc2e89eddfe9e655d4a45bec757d
James Brown <jamesphone180@gmail.com>
2018-07-26 00:15:37 +1200

    Added testing utilities and write tests.

---
+++ src/test/se306group8/scheduleoptimizer/algorithm/TestScheduleUtils.java
@@ -0,0 +1,28 @@
+package se306group8.scheduleoptimizer.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import se306group8.scheduleoptimizer.taskgraph.Task;
+import se306group8.scheduleoptimizer.taskgraph.TaskGraph;
+import se306group8.scheduleoptimizer.taskgraph.TestGraphUtils;
+
+public class TestScheduleUtils {
+	public static Schedule createTestScheduleA() {
+		TaskGraph graph = TestGraphUtils.buildTestGraphA();
+		
+		int numberOfProcessors = 2;
+		List<List<Task>> lists = Stream.generate(ArrayList<Task>::new).limit(numberOfProcessors).collect(Collectors.toList());
+		
+		int i = 0;
+		//Topological Order
+		for(Task t : graph.getAll()) {
+			lists.get(i % numberOfProcessors).add(t);
+			i++;
+		}
+		
+		return new Schedule(graph, lists);
+	}
+}
