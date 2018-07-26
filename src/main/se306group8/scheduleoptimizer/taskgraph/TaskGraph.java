21f55d9f165f6fbfee2612d273270a6f1e3323db
James Brown <jamesphone180@gmail.com>
2018-07-26 12:12:45 +1200

    Implemented TaskGraph

--- src/main/se306group8/scheduleoptimizer/taskgraph/TaskGraph.java
+++ src/main/se306group8/scheduleoptimizer/taskgraph/TaskGraph.java
@@ -1,7 +1,10 @@
 package se306group8.scheduleoptimizer.taskgraph;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 
@@ -9,34 +12,63 @@ import java.util.List;
  *
  */
 public final class TaskGraph {
+	private final List<Task> topologicalOrder;
+	private final Collection<Task> roots;
+	private final Collection<Dependency> edges;
+	private final String name;
+	
 	TaskGraph(String name, Collection<Task> tasks) {
+		assert tasks.stream().noneMatch(Objects::isNull);
 		
+		topologicalOrder = new ArrayList<>();
+		roots = new ArrayList<>();
+		this.name = name;
+		
+		for(Task task : tasks) {
+			addTask(task);
+			if(task.getParents().isEmpty()) {
+				roots.add(task);
+			}
+		}
+		
+		edges = tasks.stream()
+				.flatMap(t -> t.getChildren().stream())
+				.collect(Collectors.toList());
+	}
+	
+	private void addTask(Task parent) {
+		assert parent != null;
+		
+		for(Dependency dep : parent.getParents()) {
+			Task task = dep.getSource();
+			if(!topologicalOrder.contains(task)) {
+				addTask(task);
+			}
+		}
+		
+		topologicalOrder.add(parent);
 	}
 	
 	/**
 	 * Returns a List containing a topological ordering of all tasks.
 	 */
 	public List<Task> getAll() {
-		assert false : "Not done yet";
-		return null;
+		return topologicalOrder;
 	}
 	
 	/**
 	 * Returns a Collection of all tasks with no dependents.
 	 */
 	public Collection<Task> getRoots() {
-		assert false : "Not done yet";
-		return null;
+		return roots;
 	}
 
 	public Collection<Dependency> getEdges() {
-		// TODO Auto-generated method stub
-		return null;
+		return edges;
 	}
 
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 	
 }
