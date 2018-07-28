2b81a3a94682c86c0037bd111f7071a998514be9
James Brown <jamesphone180@gmail.com>
2018-07-28 15:05:52 +1200

    Made the test schedule generation deterministic

--- src/test/se306group8/scheduleoptimizer/dotfile/TestScheduleUtils.java
+++ src/test/se306group8/scheduleoptimizer/dotfile/TestScheduleUtils.java
@@ -1,11 +1,9 @@
 package se306group8.scheduleoptimizer.dotfile;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import se306group8.scheduleoptimizer.dotfile.ScheduleFromFile;
 import se306group8.scheduleoptimizer.taskgraph.Schedule;
 import se306group8.scheduleoptimizer.taskgraph.Task;
 import se306group8.scheduleoptimizer.taskgraph.TaskGraph;
@@ -15,14 +13,14 @@ public class TestScheduleUtils {
 	public static Schedule createTestScheduleA() {
 		TaskGraph graph = TestGraphUtils.buildTestGraphA();
 		
-		int numberOfProcessors = 2;
-		List<List<Task>> lists = Stream.generate(ArrayList<Task>::new).limit(numberOfProcessors).collect(Collectors.toList());
-		
-		int i = 0;
 		//Topological Order
-		for(Task t : graph.getAll()) {
-			lists.get(i % numberOfProcessors).add(t);
-			i++;
+		List<Task> tasks = new ArrayList<>(graph.getAll());
+		tasks.sort((a, b) -> a.getName().compareTo(b.getName()));
+		
+		List<List<Task>> lists = new ArrayList<>();
+		
+		for(Task t : tasks) {
+			lists.add(Arrays.asList(t));
 		}
 		
 		return new ScheduleFromFile(graph, lists);
