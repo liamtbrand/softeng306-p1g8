e2b141750a68eaf149d035d4a5d9f09d0c1d79fc
Liam Brand <liamtbrand@gmail.com>
2018-08-01 22:24:39 +1200

    WIP Improve Main class

--- src/main/se306group8/scheduleoptimizer/Main.java
+++ src/main/se306group8/scheduleoptimizer/Main.java
@@ -1,12 +1,22 @@
 package se306group8.scheduleoptimizer;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import se306group8.scheduleoptimizer.algorithm.Algorithm;
+import se306group8.scheduleoptimizer.algorithm.RuntimeMonitor;
 import se306group8.scheduleoptimizer.cli.ArgsParser;
 import se306group8.scheduleoptimizer.config.ArgumentException;
 import se306group8.scheduleoptimizer.config.Config;
+import se306group8.scheduleoptimizer.dotfile.DOTFileHandler;
+import se306group8.scheduleoptimizer.taskgraph.Schedule;
+import se306group8.scheduleoptimizer.taskgraph.TaskGraph;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		// Parse the arguments and get a Config object.
 		
 		ArgsParser parser = new ArgsParser();
 		Config config;
@@ -17,12 +27,42 @@ public class Main {
 			System.out.println("Problem parsing arguments: "+e.getMessage());
 			return; // Stop prematurely.
 		}
+		
+		// Read in the task graph from the input file.
+		
+		DOTFileHandler fileHandler = new DOTFileHandler();
+		
+		TaskGraph taskGraph;
+		
+		try {
+			taskGraph = fileHandler.readTaskGraph(Paths.get(config.inputFile()));
+		} catch (IOException e) {
+			System.out.println("Problem reading input file: "+e.getMessage());
+			return; // Stop prematurely.
+		}
 
-		System.out.println("inputFile = \""+config.inputFile()+"\"");
-		System.out.println("P = " + config.P());
-		System.out.println("N = " + config.N());
-		System.out.println("visualize = "+config.visualize());
-		System.out.println("outputFile = \""+config.outputFile()+"\"");
+		// Create the RuntimeMonitor.
+		
+		RuntimeMonitor monitor = null; // TODO initialize this correctly.
+		
+		// Setup the Algorithm.
+		
+		Algorithm algorithm = null; // TODO initialize this correctly.
+		
+		algorithm.setMonitor(monitor);
+		
+		// Run the Algorithm and obtain the Schedule.
+		
+		Schedule schedule = algorithm.produceCompleteSchedule(taskGraph, config.P());
+		
+		// Write the schedule to disk.
+		
+		try {
+			fileHandler.write(Paths.get(config.outputFile()), schedule);
+		} catch (IOException e) {
+			System.out.println("Problem writing output file: "+e.getMessage());
+			return; // Stop prematurely.
+		}
 		
 	}
 
