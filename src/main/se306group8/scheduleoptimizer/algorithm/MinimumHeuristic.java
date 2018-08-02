996280105a2b4c60fb437819f627742c09642741
James Brown <jamesphone180@gmail.com>
2018-08-02 12:10:48 +1200

    Fleshed out the schedule layout
    Programmed in a group session

--- src/main/se306group8/scheduleoptimizer/algorithm/MinimumHeuristic.java
+++ src/main/se306group8/scheduleoptimizer/algorithm/MinimumHeuristic.java
@@ -2,6 +2,6 @@ package se306group8.scheduleoptimizer.algorithm;
 
 @FunctionalInterface
 public interface MinimumHeuristic {
-	/** Provides an lower bound of any full solution that can be produced from this partial solution. */
-	int estimate(PartialSchedule schedule);
+	/** Provides an lower bound of any partial solution that can be produced from this partial solution. */
+	int estimate(TreeSchedule schedule);
 }
