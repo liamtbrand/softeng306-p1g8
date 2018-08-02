5e4ca1c83afe1ab6d6f2f26ba60213585d628774
Molly Farrant <mollybfarrant@gmail.com>
2018-08-03 10:26:30 +1200

    Fixed number of processors issue.

--- src/main/se306group8/scheduleoptimizer/algorithm/GreedyChildScheduleFinder.java
+++ src/main/se306group8/scheduleoptimizer/algorithm/GreedyChildScheduleFinder.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class GreedyChildScheduleFinder extends BasicChildScheduleFinder {
 
+	public GreedyChildScheduleFinder(int processors) {
+		super(processors);
+		// TODO Auto-generated constructor stub
+	}
+
 	public List<TreeSchedule> getChildSchedules(TreeSchedule schedule) {
 		Collection<TreeSchedule> unorderedSet = super.getChildSchedules(schedule);
 		List<TreeSchedule> unorderedList = (List<TreeSchedule>)unorderedSet;
