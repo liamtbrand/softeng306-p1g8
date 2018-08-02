087fb341fadb1cb8883062a56b7c77d1314066b0
Molly Farrant <mollyfarrant@Mollys-MacBook-Pro.local>
2018-08-02 18:31:46 +1200

    Implemented draft recursive BnB algorithm

--- src/main/se306group8/scheduleoptimizer/algorithm/branch_bound/BranchBoundSchedulingAlgorithm.java
+++ src/main/se306group8/scheduleoptimizer/algorithm/branch_bound/BranchBoundSchedulingAlgorithm.java
@@ -1,6 +1,5 @@
 package se306group8.scheduleoptimizer.algorithm.branch_bound;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import se306group8.scheduleoptimizer.algorithm.Algorithm;
@@ -10,43 +9,47 @@ import se306group8.scheduleoptimizer.algorithm.ListSchedule;
 import se306group8.scheduleoptimizer.algorithm.RuntimeMonitor;
 import se306group8.scheduleoptimizer.algorithm.TreeSchedule;
 import se306group8.scheduleoptimizer.taskgraph.Schedule;
-import se306group8.scheduleoptimizer.taskgraph.Task;
 import se306group8.scheduleoptimizer.taskgraph.TaskGraph;
 
 public class BranchBoundSchedulingAlgorithm implements Algorithm {
 	
+	// The best COMPLETE schedule found so far
+	private TreeSchedule _currentBestSchedule = null;
+	private int _currentBest = Integer.MAX_VALUE;
+	
 	@Override
 	public Schedule produceCompleteSchedule(TaskGraph graph, int numberOfProcessors) {
 		
-		GreedyChildScheduleFinder greedyFinder = new GreedyChildScheduleFinder();
 		TreeSchedule initialSchedule = new TreeSchedule(graph, graph.getAll().get(0), 0, new CriticalPathHeuristic());
 		
-		List<TreeSchedule> activeSchedules = new ArrayList<TreeSchedule>();
-		activeSchedules.add(initialSchedule);
-		int best = Integer.MAX_VALUE;
-		TreeSchedule currentBest = null;
+		TreeSchedule optimalSchedule = branchAndBound(initialSchedule);
 		
-		while (!activeSchedules.isEmpty()) {
-			TreeSchedule selectedSchedule = activeSchedules.get(0);
-			activeSchedules.remove(selectedSchedule);
-			List<TreeSchedule> childSchedules = greedyFinder.getChildSchedules(selectedSchedule);
-			for (TreeSchedule child : childSchedules) {
-				// check if estimate better than best
-				if (child.getLowerBound() < best) {
-					// check if child is a full schedule
-					if (child.getFullSchedule() != null) {
-					best = child.getLowerBound();
-					currentBest = child;
-					} else {
-					activeSchedules.add(child);
-					}
+		return new ListSchedule(graph, optimalSchedule.computeTaskLists());
+	}
+
+	private TreeSchedule branchAndBound(TreeSchedule schedule) {
+		
+		GreedyChildScheduleFinder greedyFinder = new GreedyChildScheduleFinder();
+		
+		// Get all children in order from best lower bound to worst
+		List<TreeSchedule> childSchedules = greedyFinder.getChildSchedules(schedule);
+		
+		for (TreeSchedule child : childSchedules) {
+			int childLowerBound = child.getLowerBound();
+			// Only consider the child if its lower bound is better than current best
+			if (childLowerBound < _currentBest) {
+				// Check if the child schedule is complete or not
+				if (child.getFullSchedule() != null) {
+					_currentBestSchedule = child;
+					_currentBest = childLowerBound;
+				} else {
+					branchAndBound(child);
 				}
 			}
 		}
-		
-		return new ListSchedule(graph, currentBest.computeTaskLists());
+		return _currentBestSchedule;
 	}
-
+	
 	@Override
 	public void setMonitor(RuntimeMonitor monitor) {
 		// TODO Auto-generated method stub
