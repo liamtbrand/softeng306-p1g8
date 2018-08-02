60829f26055679fd7fa5423c84b4d32a7c6e2c51
James Brown <jamesphone180@gmail.com>
2018-08-03 10:35:00 +1200

    Added noidletime

--- src/main/se306group8/scheduleoptimizer/algorithm/heuristic/NoIdleTimeHeuristic.java
+++ src/main/se306group8/scheduleoptimizer/algorithm/heuristic/NoIdleTimeHeuristic.java
@@ -3,11 +3,18 @@ package se306group8.scheduleoptimizer.algorithm.heuristic;
 import se306group8.scheduleoptimizer.algorithm.TreeSchedule;
 
 public class NoIdleTimeHeuristic implements MinimumHeuristic {
-
+	private final int processors;
+	
+	public NoIdleTimeHeuristic(int processors) {
+		this.processors = processors;
+	}
+	
 	@Override
 	public int estimate(TreeSchedule schedule) {
-		// TODO Auto-generated method stub
-		return 0;
+		int idleTime = schedule.getIdleTime();
+		int totalTime = schedule.getGraph().getAll().stream().mapToInt(task -> task.getCost()).sum();
+		
+		//Ceil div
+		return (totalTime + idleTime - 1) / processors + 1;
 	}
-
 }
