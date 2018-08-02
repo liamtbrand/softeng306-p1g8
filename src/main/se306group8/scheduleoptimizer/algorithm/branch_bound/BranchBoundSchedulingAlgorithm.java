774f7afe620ccc632d8e049f1690ea83186e2aa6
Molly Farrant <mollyfarrant@wifi-172-23-147-36.uoa-wifi.auckland.ac.nz>
2018-08-03 09:03:01 +1200

    Added full(ish) implementation of branchAndBound()

--- src/main/se306group8/scheduleoptimizer/algorithm/branch_bound/BranchBoundSchedulingAlgorithm.java
+++ src/main/se306group8/scheduleoptimizer/algorithm/branch_bound/BranchBoundSchedulingAlgorithm.java
@@ -13,41 +13,37 @@ import se306group8.scheduleoptimizer.taskgraph.TaskGraph;
 
 public class BranchBoundSchedulingAlgorithm implements Algorithm {
 	
-	// The best COMPLETE schedule found so far
-	private TreeSchedule _currentBestSchedule = null;
-	private int _currentBest = Integer.MAX_VALUE;
-	
 	@Override
 	public Schedule produceCompleteSchedule(TaskGraph graph, int numberOfProcessors) {
 		
 		TreeSchedule initialSchedule = new TreeSchedule(graph, graph.getAll().get(0), 0, new CriticalPathHeuristic());
 		
-		TreeSchedule optimalSchedule = branchAndBound(initialSchedule);
-		
-		return new ListSchedule(graph, optimalSchedule.computeTaskLists());
+		return branchAndBound(initialSchedule, null);
 	}
 
-	private TreeSchedule branchAndBound(TreeSchedule schedule) {
-		
+	private Schedule branchAndBound(TreeSchedule schedule, Schedule best) {
+	
 		GreedyChildScheduleFinder greedyFinder = new GreedyChildScheduleFinder();
 		
 		// Get all children in order from best lower bound to worst
 		List<TreeSchedule> childSchedules = greedyFinder.getChildSchedules(schedule);
 		
 		for (TreeSchedule child : childSchedules) {
-			int childLowerBound = child.getLowerBound();
-			// Only consider the child if its lower bound is better than current best
-			if (childLowerBound < _currentBest) {
-				// Check if the child schedule is complete or not
-				if (child.getFullSchedule() != null) {
-					_currentBestSchedule = child;
-					_currentBest = childLowerBound;
-				} else {
-					branchAndBound(child);
-				}
+			if (best == null && child.getFullSchedule() != null) {
+				best = new ListSchedule(schedule.getGraph(), child.computeTaskLists());
 			}
+			// Only consider the child if its lower bound is better than current best
+			if (child.getLowerBound() < best.getTotalRuntime()) {
+				if (child.getFullSchedule() != null) {
+					best = new ListSchedule(schedule.getGraph(), child.computeTaskLists());
+				} else {
+					// Check if the child schedule is complete or not
+					best = branchAndBound(child, best);
+				}
+			} 
 		}
-		return _currentBestSchedule;
+		
+		return best;
 	}
 	
 	@Override
