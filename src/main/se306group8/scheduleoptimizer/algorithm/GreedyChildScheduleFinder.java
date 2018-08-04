308371d79c03b604a935b3f7660c0084cb4d5d4c
Robert Rewcastle <robbierew@gmail.com>
2018-08-05 11:39:32 +1200

    Added comments to GreedyChildScheduleFinder

--- src/main/se306group8/scheduleoptimizer/algorithm/GreedyChildScheduleFinder.java
+++ src/main/se306group8/scheduleoptimizer/algorithm/GreedyChildScheduleFinder.java
@@ -12,35 +12,47 @@ import se306group8.scheduleoptimizer.taskgraph.Dependency;
 import se306group8.scheduleoptimizer.taskgraph.Task;
 import se306group8.scheduleoptimizer.taskgraph.TaskGraph;
 
-public class GreedyChildScheduleFinder implements ChildScheduleFinder{
+/**
+ * Implementation of ChildScheduleFinder that orders the child schedules based
+ * on the Task's bottom level. For children of the same Task they are ordered by
+ * starting time of the new Task.
+ */
+public class GreedyChildScheduleFinder implements ChildScheduleFinder {
 	private int numProcessors;
 
 	public GreedyChildScheduleFinder(int numProcessors) {
 		this.numProcessors = numProcessors;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public List<TreeSchedule> getChildSchedules(TreeSchedule schedule) {
 		List<Task> allocatable = new ArrayList<>(schedule.getAllocatable());
 
 		// Robert's first Java lambda
 		// ordered such that largest bottom order first
-		allocatable.sort((Task t1, Task t2) -> schedule.getGraph().getBottomTime(t2) - schedule.getGraph().getBottomTime(t1));
+		allocatable.sort(
+				(Task t1, Task t2) -> schedule.getGraph().getBottomTime(t2) - schedule.getGraph().getBottomTime(t1));
 
 		List<TreeSchedule> children = new ArrayList<TreeSchedule>();
 
 		// use the schedule to get timing info
 		// I didn't use getFullSchedule() because comments says it may return null
-		//Schedule parentSchedule = new ListSchedule(schedule.getGraph(), schedule.computeTaskLists());
+		// Schedule parentSchedule = new ListSchedule(schedule.getGraph(),
+		// schedule.computeTaskLists());
 		for (Task task : allocatable) {
 			children.addAll(greedyChildren(schedule, task));
 		}
-		
+
 		return children;
 
 	}
 
 	private List<TreeSchedule> greedyChildren(TreeSchedule parentSchedule, Task task) {
+		
+		//A small optimization to reduce equivalent schedules by only allowing 1 free processor
 		int processorsToAllocate = Math.min(parentSchedule.getNumberOfUsedProcessors() + 1, numProcessors);
 
 		List<TreeSchedule> childrenSchedules = new ArrayList<>();
@@ -49,10 +61,10 @@ public class GreedyChildScheduleFinder implements ChildScheduleFinder{
 			TreeSchedule childSchedule = new TreeSchedule(parentSchedule.getGraph(), task, p, parentSchedule);
 			childrenSchedules.add(childSchedule);
 		}
-		
-		//sort by start times
+
+		// sort by start times
 		childrenSchedules.sort((t1, t2) -> t1.getAlloctionFor(task).startTime - t2.getAlloctionFor(task).startTime);
-		
+
 		return childrenSchedules;
 	}
 }
