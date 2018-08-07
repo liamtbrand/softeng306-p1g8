01f3d59ab181dbef69f2a16da2e71239bbc0808f
James Brown <jamesphone180@gmail.com>
2018-08-07 19:15:19 +1200

    Used the new hashed ids

--- src/main/se306group8/scheduleoptimizer/algorithm/storage/ScheduleArray.java
+++ src/main/se306group8/scheduleoptimizer/algorithm/storage/ScheduleArray.java
@@ -1,20 +1,18 @@
 package se306group8.scheduleoptimizer.algorithm.storage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import se306group8.scheduleoptimizer.algorithm.TreeSchedule;
-import se306group8.scheduleoptimizer.taskgraph.Task;
 import se306group8.scheduleoptimizer.taskgraph.TaskGraph;
 
 /** This class represents a linear array of schedules */
 class ScheduleArray {
+	private static final int BLOCK_SIZE = 100_000;
+	
 	/** This class represents a large block of schedules. This represents a storage of about 1Mb.
 	 * This block size is chosen to be small enough so as to not waste memory. But large enough so
 	 * that the object overheads are largely irrelevant. */
-	private static class ScheduleBlock {
-		static final int BLOCK_SIZE = 100_000;
+	private class ScheduleBlock {
 		
 		/** Stores the array of parent schedules */
 		final int[] parentsArray = new int[BLOCK_SIZE];
@@ -24,6 +22,13 @@ class ScheduleArray {
 		final byte[] taskArray = new byte[BLOCK_SIZE];
 		/** Pre-computes the result of the heuristic */
 		final short[] lowerBound = new short[BLOCK_SIZE];
+		
+		void setSchedule(int subIndex, TreeSchedule schedule) {
+			lowerBound[subIndex] = (short) schedule.getLowerBound();
+			parentsArray[subIndex] = addSchedule(schedule.getParent());
+			taskArray[subIndex] = (byte) schedule.getMostRecentTask().getId();
+			processorArray[subIndex] = (byte) schedule.getMostRecentProcessor();
+		}
 	}
 	
 	/** Each block stores a large number of schedules at 8B per schedule. This limits the cost of resizing the array. */
@@ -32,23 +37,13 @@ class ScheduleArray {
 	
 	private int nextId = 0;
 	
-	private final Task[] integerToTask;
-	private final Map<Task, Integer> taskToInteger;
-	
 	ScheduleArray(TaskGraph graph) {
 		this.graph = graph;
-		
-		integerToTask = graph.getAll().toArray(new Task[0]);
-		taskToInteger = new HashMap<>();
-		
-		for(int i = 0; i < integerToTask.length; i++) {
-			taskToInteger.put(integerToTask[i], i);
-		}
 	}
 	
 	int getLowerBound(int id) {
-		ScheduleBlock block = blocks.get(id / ScheduleBlock.BLOCK_SIZE);
-		int subIndex = id % ScheduleBlock.BLOCK_SIZE;
+		ScheduleBlock block = blocks.get(id / BLOCK_SIZE);
+		int subIndex = id % BLOCK_SIZE;
 		return block.lowerBound[subIndex];
 	}
 	
@@ -63,23 +58,15 @@ class ScheduleArray {
 			return null;
 		}
 		
-		ScheduleBlock block = blocks.get(id / ScheduleBlock.BLOCK_SIZE);
-		int subIndex = id % ScheduleBlock.BLOCK_SIZE;
+		ScheduleBlock block = blocks.get(id / BLOCK_SIZE);
+		int subIndex = id % BLOCK_SIZE;
 		
 		int parent = block.parentsArray[subIndex];
 		int task = Byte.toUnsignedInt(block.taskArray[subIndex]);
 		int processor = Byte.toUnsignedInt(block.processorArray[subIndex]);
 		int lowerBound = Short.toUnsignedInt(block.lowerBound[subIndex]);
 		
-		return new ArrayBackedSchedule(graph, this, id, parent, getTask(task), processor, lowerBound);
-	}
-	
-	private Task getTask(int task) {
-		return integerToTask[task];
-	}
-
-	private byte getTaskID(Task task) {
-		return taskToInteger.get(task).byteValue();
+		return new ArrayBackedSchedule(graph, this, id, parent, graph.getTask(task), processor, lowerBound);
 	}
 	
 	/** Adds a schedule to the array. If this schedule object was already in the array it is not re-added
@@ -95,16 +82,11 @@ class ScheduleArray {
 		if(schedule instanceof ArrayBackedSchedule) {
 			return ((ArrayBackedSchedule) schedule).getIndex();
 		} else {
-			int parentIndex = addSchedule(schedule);
-			
 			int index = nextId++;
-			ScheduleBlock block = getBlockCheckingForBounds(index / ScheduleBlock.BLOCK_SIZE);
-			int subIndex = index % ScheduleBlock.BLOCK_SIZE;
+			ScheduleBlock block = getBlockCheckingForBounds(index / BLOCK_SIZE);
+			int subIndex = index % BLOCK_SIZE;
 			
-			block.lowerBound[subIndex] = (short) schedule.getLowerBound();
-			block.parentsArray[subIndex] = parentIndex;
-			block.taskArray[subIndex] = getTaskID(schedule.getMostRecentTask());
-			block.processorArray[subIndex] = (byte) schedule.getMostRecentProcessor();
+			block.setSchedule(subIndex, schedule);
 			
 			return index;
 		}
