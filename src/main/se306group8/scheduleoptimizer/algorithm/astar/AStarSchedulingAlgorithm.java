50149f258137a936f95830cedac28c0fed45aa6c
Robert Rewcastle <robbierew@gmail.com>
2018-08-09 20:01:26 +1200

    A star now precomputes greedy and removes children that have high estimate

--- src/main/se306group8/scheduleoptimizer/algorithm/astar/AStarSchedulingAlgorithm.java
+++ src/main/se306group8/scheduleoptimizer/algorithm/astar/AStarSchedulingAlgorithm.java
@@ -1,9 +1,14 @@
 package se306group8.scheduleoptimizer.algorithm.astar;
 
+import java.util.Collection;
+import java.util.List;
+
 import se306group8.scheduleoptimizer.algorithm.Algorithm;
 import se306group8.scheduleoptimizer.algorithm.RuntimeMonitor;
 import se306group8.scheduleoptimizer.algorithm.TreeSchedule;
 import se306group8.scheduleoptimizer.algorithm.childfinder.ChildScheduleFinder;
+import se306group8.scheduleoptimizer.algorithm.childfinder.GreedyChildScheduleFinder;
+import se306group8.scheduleoptimizer.algorithm.greedy.GreedySchedulingAlgorithm;
 import se306group8.scheduleoptimizer.algorithm.heuristic.MinimumHeuristic;
 import se306group8.scheduleoptimizer.algorithm.storage.ScheduleStorage;
 import se306group8.scheduleoptimizer.taskgraph.Schedule;
@@ -36,8 +41,42 @@ public class AStarSchedulingAlgorithm extends Algorithm {
 		ScheduleStorage queue = new ScheduleStorage(storageSizeLimit);
 		TreeSchedule best = new TreeSchedule(graph, heuristic);
 		
+		GreedyChildScheduleFinder greedyFinder = new GreedyChildScheduleFinder(numberOfProcessors);
+		
+		TreeSchedule greedySoln = best;
+		while (!greedySoln.isComplete()) {
+			greedySoln = greedyFinder.getChildSchedules(greedySoln).get(0);
+		}
+		
+		queue.storeSchedule(greedySoln);
+		int upperBound = greedySoln.getRuntime();
+		
 		while(!best.isComplete()) {
-			queue.storeSchedules(childGenerator.getChildSchedules(best));
+			
+			List<TreeSchedule> children = childGenerator.getChildSchedules(best);
+			
+			//if one child is complete they are all complete
+			if (children.get(0).isComplete()) {
+				
+				//sort by lowest runtime
+				children.sort((TreeSchedule s1, TreeSchedule s2) -> s1.getRuntime() - s2.getRuntime());
+				TreeSchedule completeSchedule = children.get(0);
+				
+				//if false all children are useless
+				if (completeSchedule.getRuntime() < upperBound) {
+					queue.storeSchedule(completeSchedule);
+					upperBound  = completeSchedule.getRuntime();
+				}
+				
+			}else {
+				for (TreeSchedule partial:children ) {
+					if (partial.getLowerBound() < upperBound) {
+						queue.storeSchedule(partial);
+					}
+				}
+			}
+			
+			
 			best = queue.getBestSchedule();
 			getMonitor().setSolutionsExplored(queue.size());
 		}
