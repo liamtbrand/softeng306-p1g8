fdeddb1cf37f85f3a697914888e393a7394b8d7e
James Brown <jamesphone180@gmail.com>
2018-08-17 17:48:29 +1200

    The application now terminates

--- src/main/se306group8/scheduleoptimizer/visualisation/controller/MainController.java
+++ src/main/se306group8/scheduleoptimizer/visualisation/controller/MainController.java
@@ -42,6 +42,7 @@ public class MainController implements Initializable {
 		taskGraphPageController.stop();
 		searchSpacePageController.stop();
 		consolePageController.stop();
+		histogramPageController.stop();
 	}
 
 }
